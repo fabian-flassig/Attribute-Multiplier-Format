d2e90c3119857eda2ba3f80f3f5676f5a0a7214b
fabian-flassig <64731310+fabian-flassig@users.noreply.github.com>
2022-05-20 10:28:37 +0200

    Initial commit

---
+++ source.lua
@@ -0,0 +1,10 @@
+-- Main entry point for the attribute
+function evaluate_multiplier(element)
+	local multiplier = element:get_element_attribute("58") or ""
+	if name == "" then
+		return ""
+	end
+	
+	
+	return string.format('%02d', multiplier)
+end
